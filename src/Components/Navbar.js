af4f49864d4412ff07b988fe75aa63890f5a4fbe
Purvi Saini <purvitashu450@gmail.com>
2023-09-07 14:55:40 +0530

    navbar corrected | jquery added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-0 mt-1">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-0">
   <div className="container-fluid fs-6">
     <a className="navbar-brand" href="#home">
       PS
@@ -20,9 +20,9 @@ function Navbar() {
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link " aria-current="page" href="#home">
+          <a className="nav-link" aria-current="page" href="#home">
             Home
           </a>
         </li>
