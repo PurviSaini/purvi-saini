1badcf4ff23548f03a5923afd6b93769fd631b43
Purvi Saini <purvitashu450@gmail.com>
2023-09-10 01:30:46 +0530

    basic Education structure added

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,6 +1,40 @@
 function Education(){
     return(
-        <div className="my-5"></div>
+        <>
+        <h2>Education</h2>
+        <div className="my-5 d-flex justify-content-evenly align-items-center flex-wrap" id="education">
+            <div className="card" style={{ width: "18rem"}}>
+                <div className="card-body" style={{ backgroundColor:"#231E2C" }}>
+                <h5 className="card-title">Card title</h5>
+                <h6 className="card-subtitle mb-2 text-muted" style={{ backgroundColor:"#231E2C" }}>Card subtitle</h6>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk
+                    of the card's content.
+                </p>
+                </div>
+            </div>
+            <div className="card" style={{ width: "18rem" }}>
+                <div className="card-body" style={{ backgroundColor:"#231E2C" }}>
+                <h5 className="card-title">Card title</h5>
+                <h6 className="card-subtitle mb-2 text-muted" style={{ backgroundColor:"#231E2C" }}>Card subtitle</h6>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk
+                    of the card's content.
+                </p>
+                </div>
+            </div>
+            <div className="card" style={{ width: "18rem" }}>
+                <div className="card-body" style={{ backgroundColor:"#231E2C" }}>
+                <h5 className="card-title">Card title</h5>
+                <h6 className="card-subtitle mb-2 text-muted" style={{ backgroundColor:"#231E2C" }}>Card subtitle</h6>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make up the bulk
+                    of the card's content.
+                </p>
+                </div>
+            </div>
+        </div>
+        </>
     );
 }
 export default Education;
